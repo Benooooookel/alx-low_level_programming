30d34875c1260e1f5dfa5fc0cf47f5d7fc685829
kelechi nnadi <kelechinnadi019@gmail.com>
2023-07-15 01:56:39 +0100

    the alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - checks the code
+ *
+ * Return: alway 0
+ */
+int main(void)
+{
+	char alph = 'a';
+
+	while (alph < 'z')
+	{
+		putchar(alph);
+		alph++;
+	}
+	putchar('\n');
+	return (0);
+}
