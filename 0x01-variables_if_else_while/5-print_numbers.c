b42fe65d06fc341244cd2ce2383c80c8ec8d2855
kelechi nnadi <kelechinnadi019@gmail.com>
2023-07-15 11:24:36 +0100

    the numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
