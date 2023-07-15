a4be1cca2e0514f58d11d64ff288fba3e9d476b0
kelechi nnadi <kelechinnadi019@gmail.com>
2023-07-15 11:18:44 +0100

    the alpa sub

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - function that checks code that print all except q and e
+ *
+ * Return: alway 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'q' ||  i == 'e')
+			continue;
+		putchar(i);
+	}	
+	putchar('\n');
+	return (0);
+}
