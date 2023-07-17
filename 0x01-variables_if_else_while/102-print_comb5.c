c7303bdae72ad1354be437585b332c4624b19601
kelechi nnadi <kelechinnadi019@gmail.com>
2023-07-17 16:57:05 +0100

    the numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,5 +6,23 @@
  */
 int main(void)
 {
+	int a, b;
 
+	for (a = 1; a < 9; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a >= 9 && b >= 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}	
+	}
+	putchar('\n');
+	return (0);
 }
