8d0790dedb95ce86511d6c6da6efd0aad7904c23
kelechi nnadi <kelechinnadi019@gmail.com>
2023-07-20 16:47:04 +0100

    the code

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,12 +11,21 @@ void times_table(void)
 
 	for (a = 0; a < 10; a++)
 	{
-		for (b = 0;  b < 10; b++)
+		_putchar('0');
+		for (b = 1;  b < 10; b++)
 		{
+			_putchar(',');
+			_putchar(' ');
+
 			result = a * b;
-			printf("%d, ", result);
+
+			if (result < 9)
+				_putchar(' ');
+			else
+				_putchar((result / 10) + '0');
+			_putchar((result % 10) + '0');
 		}
-		printf("\n");
+		_putchar('\n');
 
 	}
 }
