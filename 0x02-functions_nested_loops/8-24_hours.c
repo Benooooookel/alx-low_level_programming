1caafbfbe77b112023244b5cfc422948874b9d62
kelechi nnadi <kelechinnadi019@gmail.com>
2023-07-20 17:22:12 +0100

    the update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,7 +11,7 @@ void jack_bauer(void)
 
 	for (hour = 0; hour <= 23; hour++)
 	{
-		for(minute = 0; minute <= 59; minute++)
+		for (minute = 0; minute <= 59; minute++)
 		{
 			_putchar(hour / 10 + '0');
 			_putchar(hour % 10 + '0');
@@ -19,6 +19,6 @@ void jack_bauer(void)
 			_putchar(minute / 10 + '0');
 			_putchar(minute % 10 + '0');
 			_putchar('\n');
-		}	
+		}
 	}
 }
