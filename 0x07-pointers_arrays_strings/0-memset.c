f60f6b47a7b83fd9b53dddc708dc9a394eec339c
kelechi nnadi <kelechinnadi019@gmail.com>
2023-08-02 01:49:29 +0100

    the code

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * *_memset - function that file the memory with a constant byte
+ * @s: is the pointer to
+ * @b: is the char to be returned
+ * @n: is the number of times the charater to be shown
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+  unsinged int n;
+ 
+    
+}
