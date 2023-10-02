e84c4dc081c006eb00513d8047bc64cc2100d443
kelechi nnadi <kelechinnadi019@gmail.com>
2023-10-02 22:39:46 +0100

    the code

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - function that print all possible combination of a single digits
+ *
+ * Auth: kelechi nnadi <alx swe>
+ * Return: always 0
+ */
+int main(void)
+{
+	int comb = 0;
+
+	while (comb < 10)
+	{
+		if (comb > 9)
+			putchar(',');
+		putchar(48 + comb);
+		if (comb > 9)
+			break;
+		putchar(',');
+		putchar(' ');
+		comb++;
+	}
+	putchar('\n');
+	return (0);
+}
