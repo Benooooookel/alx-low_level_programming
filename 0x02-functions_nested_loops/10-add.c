5e69562e24f6c2ac5f55c347abcc40a236d95a63
kelechi nnadi <kelechinnadi019@gmail.com>
2023-10-05 21:37:01 +0100

    the add number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - function that add two integers and returns the result
+ * @first:  first number
+ * @sec: sec number
+ * Return: return the result of the number add
+ */
+int add(int first, int sec)
+{
+	return first + sec;
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include  "main.h"
+#include <stdlib.h>
+/**
+ * print_last_digit - function that prints the last digit of a number
+ * @n: the parameter to be printed
+ * Return: the value of the last digit
+ * Auth: kelechi nnadi <alx swe>
+ */
+int print_last_digit(int n)
+{
+	int res = n % 10;
+
+	if (n < 0)
+		return (abs(res));
+}
