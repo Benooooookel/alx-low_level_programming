e09346390083abc96de2e53e1628058087da27ab
kelechi nnadi <kelechinnadi019@gmail.com>
2023-10-05 18:59:03 +0100

    the isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - function that check for alphabetic character
+ * @n: the parameter
+ * Return: if c is a letter, lowercase or uppercase returns 1
+ *        else return 0
+ * Auth: kelechi nnadi <alx swe>
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90 )|| (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
+}
