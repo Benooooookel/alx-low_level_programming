05c833d0017ba6a48e1ff84cc257500290d6c8c1
kelechi nnadi <kelechinnadi019@gmail.com>
2023-10-05 17:36:23 +0100

    the puchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - function that prints puchar with a new line using
+ *        putchar function for the printing
+ * Auth: kelechi nnaid <alx swe>
+ * Return: always 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
