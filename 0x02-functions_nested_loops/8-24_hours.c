3d29511d1b6f610221d28338ce13e0c04791eae1
kelechi nnadi <kelechinnadi019@gmail.com>
2023-10-06 16:47:17 +0100

    the clock for the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - function that prints every minute of the day of jack
+ *              Bauer, starting from 00.00 to 23:59
+ * 
+ * Auth: kelechi nnadi <alx swe>
+ */
+void jack_bauer(void)
+{
+	int min1, sec1;
+
+	for (min1 = 0; min1 <= 23; min1++)
+	{
+		for (sec1 = 0; sec1 <= 59; sec1++)
+		{
+			_putchar((min1 / 10) + '0');
+			_putchar((min1 % 10) + '0');
+			_putchar(':');
+			_putchar((sec1 / 10) + '0');
+			_putchar((sec1 % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
