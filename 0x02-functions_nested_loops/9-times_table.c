f84c32d94938490927be3e008b2d43198d877af4
kelechi nnadi <kelechinnadi019@gmail.com>
2023-10-06 17:18:07 +0100

    the time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - function that prints the 9 times table, starting with 0
+ *
+ * Auth: kelechi nnadi <alx swe>
+ */
+void times_table(void)
+{
+	int width, col;
+	
+	for (width = 0; width <= 9; width++)
+	{
+		for (col = 0 * 1; col <= 9; col++)
+		{
+			_putchar(width + '0');
+			_putchar(',');
+			_putchar(' ');
+			_putchar(col + '0');
+		}
+		_putchar('\n');
+	}
+}
