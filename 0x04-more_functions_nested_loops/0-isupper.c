5d639eb21f5ae4191e5cf87c12197d8368fd15b1
kelechi nnadi <kelechinnadi019@gmail.com>
2023-10-09 06:20:50 +0100

    the isdigit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,14 @@
-/**
- * file: 0-isupper.c
- * Auth: kelechi nnadi <alx swe>
- */
 #include <ctype.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _isupper - function that checks for uppercase character
- * @c: the parameter to be called on
- * Return: checks if c is uppercase returns 1 else return 0
+ * _isupper - function that checks for  uppercase character
+ * @c: the parameter number
+ * Return: if c is uppercase return 1 else return 0
  */
-int  _isupper(int c)
+
+int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 		return (1);
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+/**
+ * file: 1-isdigit.c
+ * Auth: kelechi nnadi <alx-swe>
+ */
+
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isdigit - function that checks for a digit 0 through 9
+ * @c: the parameter
+ * Return: if c is digit return 1 otherwise return 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
