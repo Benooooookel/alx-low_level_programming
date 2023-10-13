49f048709ac488f28674558dd2a6bc9f522c8fc7
kelechi nnadi <kelechinnadi019@gmail.com>
2023-10-13 07:46:33 +0100

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+/*
+ * file: 0-reset_to_98.c
+ * Auth: kelechi nnadi <alx swe>
+ */
+#include "main.h"
+/**
+ * reset_to_98 - function that takes a pointer to an int as parameter
+ *               and updates the value it points to 98
+ * @n: the parameter to be add
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
