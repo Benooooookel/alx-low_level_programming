8e5cbe1f4fbec38be8dca3a81606dbd87103a14d
kelechi nnadi <kelechinnadi019@gmail.com>
2023-10-13 11:01:29 +0100

    put string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+/*
+ * file: 3-puts.c
+ * Auth: kelechi nnadi <alx swe>
+ */
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - function that prints a string, followed by a new line
+ * @str: pointer to a string
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
