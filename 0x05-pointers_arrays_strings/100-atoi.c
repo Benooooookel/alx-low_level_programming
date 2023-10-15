a94771da798dd3af609288ef84b513f05e8c5b51
kelechi nnadi <kelechinnadi019@gmail.com>
2023-10-15 07:16:07 +0100

    the code

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,28 +16,21 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1, base = 0, i = 0;
-
-	while (s[i] == ' ')
+	int res,i;
+	int len = strlen(s);
+	for (i = 0; i <= len; i++)
 	{
-		i++;
+		if (s[i] == ' ')
+			res = s[i++];
+		else if (s[i] == '+' || s[i] == '-')
+			res = s[i++];
+		else
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				res = s[i];
+				i++;
+			}
 	}
-	if (s[i] == '-' || s[i] == '+')
-	{
-		sign = 1 - 2 * (s[i++] == '-');
-	}
-
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		if (base > INT_MAX  / 10 
-				|| (base == INT_MAX / 10 && s[i] == '0' > 7))
-		{
-			if (sign == 1)
-				return INT_MAX;
-			else
-				return INT_MIN;
-		}
-		base = 10 * base + (s[i++] - '0');
-	}
-	return base * sign;
+	res = atoi(s);
+	return (res);
 }
