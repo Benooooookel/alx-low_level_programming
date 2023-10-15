126625ba5335dc2674321597d28ca91b3aa3861d
kelechi nnadi <kelechinnadi019@gmail.com>
2023-10-15 09:29:31 +0100

    the strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,10 +9,11 @@
  * _strcmp - function that compares two string
  * @s1: the first string pointer to be compared
  * @s2: thes second string pointer to be compared
- * Return: three different values 
+ * Return: three different values
  */
 int _strcmp(char *s1, char *s2)
 {
 	int res = strcmp(s1, s2);
+
 	return (res);
 }
