00dace702a0e67bdd598de776d39a22704f90868
kelechi nnadi <kelechinnadi019@gmail.com>
2023-11-29 05:50:34 +0100

    the code

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include "dog.h"
+#include <stdio.h>
+
 /**
  * print_dog - function that prints a struct dog
  * @d: the variable to print
@@ -7,13 +9,10 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		print(NULL);
-
-	if (d == NULL)
 	{
-		print(NULL);
+		return;
 	}
-	print("Name:", d->name);
-	print("Age:", d->age);
-	print("Owner:", d->owner);
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
