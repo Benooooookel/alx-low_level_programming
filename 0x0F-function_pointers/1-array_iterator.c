5e6378c96050ab0d3f2431230e974f378c4bacd7
kelechi nnadi <kelechinnadi019@gmail.com>
2023-11-30 23:29:25 +0100

    the new update

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * array_iterator - function that executes given as a parameter
@@ -11,7 +12,10 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
-
+	if (array == NULL && action == NULL)
+	{
+		exit(1);
+	}
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
