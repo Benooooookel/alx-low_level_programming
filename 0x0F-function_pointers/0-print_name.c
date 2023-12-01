f072a57024db9cd78b116ecabbb2c17b25eb41d3
kelechi nnadi <kelechinnadi019@gmail.com>
2023-12-01 01:26:10 +0100

    the code

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  * print_name - function that prints a name
  * @name: the name of the function
@@ -8,4 +9,6 @@
 void print_name(char *name, void (*f)(char *))
 {
 	(f(name));
+
+	exit(0);
 }
