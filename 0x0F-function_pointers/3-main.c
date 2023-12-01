72423b5d59d5d71e3f021037cf39cbc4a243b8ae
kelechi nnadi <kelechinnadi019@gmail.com>
2023-12-01 02:46:53 +0100

    the code

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,13 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * main - function that checks code
+ * Return: always 0
+ */
+int main(void)
+{
+	int num1, num2;
+
+	i = atoi();
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+
+/**
+ * op_add - function that adds value a to value b
+ * @a: the first operator
+ * @b: the second operator
+ * Return: the sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function that subtract value a and b
+ * @a: the first operator
+ * @b: the second operator
+ * Return: the sum
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function that subtract value a and b
+ * @a: the first operator
+ * @b: the second operator
+ * Return: the sum
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function that multiply value a and b
+ * @a: the first operator
+ * @b: the second operator
+ * Return: the sum
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - function that modlus the value a and b
+ * @a: the first operator
+ * @b: the second operator
+ * Return: the sum
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
