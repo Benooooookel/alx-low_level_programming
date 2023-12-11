69b050f95699674106e0538acc2f8e825a99eebd
kelechi nnadi <kelechinnadi019@gmail.com>
2023-12-11 22:38:13 +0100

    the code

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	 const listint_t *ptr;
-	 size_t count = 0;
+	const listint_t *ptr;
+	size_t count = 0;
 
 	ptr = h;
 
 	while (ptr != NULL)
 	{
-		printf("%d\n", ptr->n);
+		if (ptr)
+			printf("%d\n", ptr->n);
 		ptr = ptr->next;
 		count++;
 	}
